d469b43aadb5eca6e6bfa69abb66bdfd5222af20
AlekseyKurnakov <kurnakov.a.v@mail.ru>
2026-04-05 08:01:48 +0300

    add Checkstyle

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/AlekseyKurnakov/java-project-78/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/AlekseyKurnakov/java-project-78/actions)
+[![Checkstyle](https://github.com/AlekseyKurnakov/java-project-78/actions/workflows/checkstyle.yml/badge.svg)](https://github.com/AlekseyKurnakov/java-project-78/actions/workflows/checkstyle.yml)
---
+++ app/build.gradle.kts
@@ -0,0 +1,34 @@
+plugins {
+    id("java")
+    checkstyle
+    id("org.sonarqube") version "7.2.3.7755"
+}
+
+group = "hexlet.code"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+checkstyle {
+    toolVersion = "10.17.0"
+    configFile = file("config/checkstyle/checkstyle.xml")
+    isIgnoreFailures = false
+}
+sonar {
+    properties {
+        property("sonar.projectKey", "AlekseyKurnakov_java-project-78")
+        property("sonar.organization", "alekseykurnakov")
+    }
+}
