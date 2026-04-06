cc8687d162ba18737f9e62c6a5e3eed5723d82e9
AlekseyKurnakov <kurnakov.a.v@mail.ru>
2026-04-06 08:03:10 +0300

    created classes Validator and StringSchema

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,4 +1,5 @@
 plugins {
+    jacoco
     id("java")
     checkstyle
     id("org.sonarqube") version "7.2.3.7755"
@@ -15,6 +16,11 @@ dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+    compileOnly("org.projectlombok:lombok:1.18.44")
+    annotationProcessor("org.projectlombok:lombok:1.18.44")
+    testCompileOnly("org.projectlombok:lombok:1.18.44")
+    testAnnotationProcessor("org.projectlombok:lombok:1.18.44")
 }
 
 tasks.test {
@@ -30,6 +36,10 @@ sonar {
     properties {
         property("sonar.projectKey", "AlekseyKurnakov_java-project-78")
         property("sonar.organization", "alekseykurnakov")
+        property("sonar.coverage.jacoco.xmlReportPaths", "build/reports/jacoco/test/jacocoTestReport.xml")
     }
+}
+jacoco {
+    toolVersion = "0.8.14"
 }
 
---
+++ app/src/main/java/hexlet/code/Validator.java
@@ -0,0 +1,11 @@
+package hexlet.code;
+
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+public class Validator {
+
+    public StringSchema string() {
+        return new StringSchema();
+    }
+}
