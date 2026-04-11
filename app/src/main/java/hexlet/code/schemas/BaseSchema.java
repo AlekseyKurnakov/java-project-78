7e3b0cbbbf620dbd9aa0542534a27296b24fb15f
AlekseyKurnakov <kurnakov.a.v@mail.ru>
2026-04-11 15:01:52 +0300

    Remove IDEA files from repository

--- app/src/main/java/hexlet/code/schemas/BaseSchema.java
+++ app/src/main/java/hexlet/code/schemas/BaseSchema.java
@@ -7,11 +7,11 @@ import java.util.function.Predicate;
 public class BaseSchema<T> {
     private final Map<String, Predicate<T>> validators = new HashMap<>();
 
-    protected void addValidator(String name, Predicate<T> validator) {
+    protected final void addValidator(String name, Predicate<T> validator) {
         validators.put(name, validator);
     }
 
-    public boolean isValid(T value) {
+    public final boolean isValid(T value) {
         for (var validator : validators.values()) {
             if (!validator.test(value)) {
                 return false;
--- app/src/main/java/hexlet/code/schemas/StringSchema.java
+++ app/src/main/java/hexlet/code/schemas/StringSchema.java
@@ -1,7 +1,7 @@
 package hexlet.code.schemas;
 
 
-public class StringSchema extends BaseSchema<String> {
+public final class StringSchema extends BaseSchema<String> {
 
     public StringSchema required() {
         addValidator("required", value -> value != null && !value.isEmpty());
